2655547664490aa126615a35ea820753960f30e6
yhoon <yhoon3002@naver.com>
2022-08-05 00:16:22 +0900

    02_심화 캐릭터 움직이기 (달리기, 점프, 앉기)

--- Survival Game/Assets/Scripts/PlayerController.cs
+++ Survival Game/Assets/Scripts/PlayerController.cs
@@ -4,19 +4,45 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // 스피드 조정 변수
     [SerializeField]
     private float walkSpeed;
+    [SerializeField]
+    private float runSpeed;
+    private float applySpeed;
+    [SerializeField]
+    private float crouchSpeed;
+
+    // 점프 
+    [SerializeField]
+    private float jumpForce;
+
+    // 상태 변수 (걷는건지 뛰는건지 구분)
+    // isGround를 설정해주는 이유 : 땅에서만 점프가 되게 하기 위해 / 안해주면 공중에서 또 한번 점프가 될 수 있기 때문
+    private bool isRun = false;
+    private bool isCrouch = false;
+    private bool isGround = true;
+
+    // 앉았을 때 얼마나 앉을지 결정하는 변수
+    [SerializeField]
+    private float crouchPosY;
+    private float originPosY;
+    private float applyCrouchPosY;
+
+    // 땅 착지 여부
+    private CapsuleCollider capsuleCollider;
 
     // 카메라의 민감도
     [SerializeField]
     private float lookSensitivity;
 
-    // 카메라의 각도
+    // 카메라의 각도의 한계
     [SerializeField]
     private float cameraRotationLimit;
     // 0은 생략해도 됨 -> 기본값이 0임
     private float currentCameraRotationX = 0;
 
+    // 필요한 컴포넌트
     [SerializeField]
     private Camera theCamera;
     // Rigidbody : 캐릭터의 실제 육체적인 몸
@@ -26,20 +52,149 @@ public class PlayerController : MonoBehaviour
     // 스크립트가 처음 실행되고 실행되는 것
     void Start()
     {
+        capsuleCollider = GetComponent<CapsuleCollider>();
         // FindObjectOfType : 모든 객체를 조사하여 Camera라는 것을 찾게 됨. 하지만, Camera가 여러 개 일수도 있으므로, 이 방법은 사용하지 않음.
         // theCamera = FindObjectOfType<Camera>();
-        myRigid = GetComponent<Rigidbody>();   
+        myRigid = GetComponent<Rigidbody>();
+        applySpeed = walkSpeed;
+
+        // 초기화
+        originPosY = theCamera.transform.localPosition.y;
+        applyCrouchPosY = originPosY;
     }
 
     // Update is called once per frame
     // 1초에 보통 60회 정도 업데이트됨
     void Update()
     {
+        IsGround();
+        TryJump();
+        // 뛰는지 걷는지 움직이기 전에 판단해야 하므로 Move() 함수 위에 있어야 함
+        TryRun();
+        TryCrouch();
         Move();
         CameraRotation();
         CharacterRotation();
     }
 
+    // 앉기 시도
+    private void TryCrouch()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            Crouch();
+        }
+    }
+
+    // 앉기 동작
+    private void Crouch()
+    {   
+        // isCrouch가 true이면 false / false이면 true로 바꿔줌
+        isCrouch = !isCrouch;
+
+        if (isCrouch)
+        {
+            applySpeed = crouchSpeed;
+            applyCrouchPosY = crouchPosY;
+        } else
+        {
+            applySpeed = walkSpeed;
+            applyCrouchPosY = originPosY;
+        }
+
+        // x, y, z를 각각 한개씩 수정할 수 없어서 Vector3를 사용하여 바꿔주어야 한다.
+        //theCamera.transform.localPosition = new Vector3(theCamera.transform.localPosition.x, applyCrouchPosY, theCamera.transform.localPosition.z);
+
+        StartCoroutine(CrouchCoroutine());
+    }
+
+    // 병렬 처리 됨
+    // 부드러운 앉기 동작 실행
+    IEnumerator CrouchCoroutine()
+    {
+        float _posY = theCamera.transform.localPosition.y;
+        int count = 0;
+
+        while(_posY != applyCrouchPosY)
+        {
+            count++;
+            // 보간을 이용
+            _posY = Mathf.Lerp(_posY, applyCrouchPosY, 0.3f);
+            theCamera.transform.localPosition = new Vector3(0, _posY, 0);
+
+            if(count > 15)
+            {
+                break;
+            }
+            yield return null;
+        }
+        theCamera.transform.localPosition = new Vector3(0, applyCrouchPosY, 0f);
+
+    }
+
+    // 지면 체크
+    private void IsGround()
+    {
+        // Raycast(레이저를 쏠 위치, 어느 방향으로, 어느 거리만큼
+        // capsuleCollider.bounds.extents.y : capsuleCollider의 y값의 절반
+        // + 0.1f를 해주는 이유 : 계단이나 경사면에서는 땅에 닿아있는 것처럼 보이지만 약간의 오차가 발생해서 그 오차를 상쇄시키기 위해 +0.1f를 해줌
+        // 땅에 닿아있다면 true, 땅에 닿아있지 않다면 false를 반환함
+        isGround = Physics.Raycast(transform.position, Vector3.down, capsuleCollider.bounds.extents.y + 0.1f);
+    }
+
+    // 점프 시도
+    private void TryJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGround)
+        {
+            Jump();
+        }
+    }
+
+    // 점프
+    private void Jump()
+    {
+        // 앉은 상태에서 점프시 앉은 상태 해제
+        if (isCrouch)
+        {
+            Crouch();
+        }
+        myRigid.velocity = transform.up * jumpForce;
+    }
+
+    // 달리기 시도
+    private void TryRun()
+    {
+        // GetKey : Key가 계속 눌러져 있는 상태
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Running();
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            RunningCancel();
+        }
+    }
+
+    // 달리기 실행
+    private void Running()
+    {
+        if (isCrouch)
+        {
+            Crouch();
+        }
+        isRun = true;
+        applySpeed = runSpeed;
+    }
+
+    // 달리기 취소
+    private void RunningCancel()
+    {
+        isRun = false;
+        applySpeed = walkSpeed;
+    }
+
+    // 움직임 실행
     private void Move()
     {
         // Horizontal은 unity에서 기본적으로 Horizontal로 설정해두었음.
@@ -58,7 +213,9 @@ public class PlayerController : MonoBehaviour
         // (1, 0, 1) = 2
         // (0.5, 0, 0.5) = 1
         // 유니티에서 내부 알고리즘 상으로 1이 나오면 속도가 더 빠르고, 계산하기 편하게 합을 1로 만들어주는 것임
-        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * walkSpeed;
+        // applySpeed에 따라 걷는 속도와 뛰는 속도가 구분됨
+        // applySpeed가 없다면 걷는 함수와 뛰는 함수 두개를 나눠줘야 함
+        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * applySpeed;
 
         // Time.deltaTime : Update 내장 함수가 매 프레임마다 실행되기 때문에
         // 아래와 같이 만들어주어서 실행되게 함
@@ -66,9 +223,9 @@ public class PlayerController : MonoBehaviour
 
     }
 
+    // 좌우 캐릭터 회전
     private void CharacterRotation()
     {
-        // 좌우 캐릭터 회전
         float _yRotation = Input.GetAxisRaw("Mouse X");
         Vector3 _characterRotationY = new Vector3(0f, _yRotation, 0f) * lookSensitivity;
 
